b3cac660e252b18fb4e186e4f82d31620bd5bc64
rayshaimania <rayshaimania@gmail.com>
2025-12-11 18:13:50 +0700

    Add files via upload

---
+++ routes/userRoute.js
@@ -0,0 +1,14 @@
+const express = require('express') // untuk memuat router
+const userControl = require('../controlers/userController')
+
+const router = express.Router()
+
+router.get('/',userControl.getAllBooks)
+router.get('/:code',userControl.getBookByCode)
+
+router.post('/',userControl.addBook)
+router.delete('/:code',userControl.delBook)
+
+router.put('/:code', userControl.updateBook)
+
+module.exports = router
