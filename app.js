556df8e43e1ebf356fba85472dd5692ad4fd46b6
rayshaimania <rayshaimania@gmail.com>
2025-12-18 17:54:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const userRoute = require('./routes/userRoute')
 
+const logger = require('./middlewares/logger')
+const auth = require('./middlewares/userAuth')
+
 const app = express()
 
 app.use(bodyParser.json())
+app.use(logger)
+app.use(auth)
+
 app.use('/api/books',userRoute)
 
 const PORT = process.env.PORT
