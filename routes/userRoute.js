0c87f07132ab6496b6abb240cc4b3a5f92806ad9
rayshaimania <rayshaimania@gmail.com>
2025-12-18 17:53:05 +0700

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,16 @@
 const express = require('express') // untuk memuat router
-const userControl = require('../controlers/userController')
+const userControl = require('../controllers/userController')
 
 const router = express.Router()
 
 router.get('/',userControl.getAllBooks)
 router.get('/:code',userControl.getBookByCode)
 
-router.post('/',userControl.addBook)
-router.delete('/:code',userControl.delBook)
+router.get('/',userControl.getAllUser)
+router.get('/:id',userControl.getUserById)
+
+router.post('/', userControl.addBook)
+router.delete('/:code', userControl.delBook)
 
 router.put('/:code', userControl.updateBook)
 
